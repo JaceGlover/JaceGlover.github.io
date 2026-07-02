37928d3a5677dda38df4e494de83a4e3c859fb24
Jace Glover <Jaceglover240@gmail.com>
2026-07-02 20:24:07 +1200

    Update and rename helpbot.ts to helpbot.js

--- src/scripts/helpbot.js
+++ src/scripts/helpbot.js
@@ -2,28 +2,24 @@ function sendMessage() {
   const input = document.getElementById("user-input");
   if (!(input instanceof HTMLInputElement)) throw new ReferenceError();
   const userText = input.value.toLowerCase();
-
   if (userText === "") return;
-
   addMessage("user", userText);
-
   let response = getBotResponse(userText);
   setTimeout(() => addMessage("bot", response), 500);
   input.value = "";
 }
 
-function addMessage(sender: string, text: string) {
+function addMessage(sender, text) {
   const chatBox = document.getElementById("chat-box");
   const msg = document.createElement("div");
   msg.classList.add("message", sender);
   msg.textContent = text;
   if (chatBox === null) throw new ReferenceError();
   chatBox.appendChild(msg);
-
   chatBox.scrollTop = chatBox.scrollHeight;
 }
 
-function getBotResponse(input: string): string {
+function getBotResponse(input) {
   if (input.includes("what is aceblocks") || input.includes("about")) {
     return "AceBlocks is your awesome project platform 😎";
   } else if (input.includes("help")) {
